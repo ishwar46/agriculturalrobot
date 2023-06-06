0a0c7d8107aa9b606e94db7ffccc5b87d5a2ba83
Elex Maharjan <ishwarura@gmail.com>
2023-06-06 17:53:31 +0545

    App Color and Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,185 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+import 'Styles/AppColor.dart';
+
+late ConnectivityResult connectivityResult;
+late AndroidNotificationChannel channel;
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+GlobalKey<NavigatorState> gloablNavigatorKey = GlobalKey<NavigatorState>();
+String? firebaseToken = "";
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: AppColor.primary,
+    ),
+  );
+  await CheckConnectivity();
+  if (connectivityResult == ConnectivityResult.mobile) {
+    await initializeFirebase();
+  } else if (connectivityResult == ConnectivityResult.wifi) {
+    await initializeFirebase();
+  }
   runApp(const MyApp());
+  configLoading();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future CheckConnectivity() async {
+  connectivityResult = await (Connectivity().checkConnectivity());
+}
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.notification?.title}");
+}
+
+void configLoading() {
+  EasyLoading.instance
+    ..indicatorType = EasyLoadingIndicatorType.ripple
+    ..loadingStyle = EasyLoadingStyle.custom
+    ..backgroundColor = Colors.black38
+    ..indicatorColor = Colors.white
+    ..textColor = Colors.white
+    ..maskType = EasyLoadingMaskType.clear
+    ..maskColor = Colors.transparent
+    ..indicatorColor = AppColor.primary
+    ..textStyle = const TextStyle(
+      color: Colors.white,
+    )
+    ..userInteractions = false
+    ..fontSize = 12
+    ..indicatorSize = 30.0
+    ..dismissOnTap = false
+    ..animationStyle = EasyLoadingAnimationStyle.scale
+    ..toastPosition = EasyLoadingToastPosition.bottom;
+}
+
+Future initializeFirebase() async {
+  try {
+    await Firebase.initializeApp();
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    try {
+      messaging.getToken().then((value) {
+        firebaseToken = value;
+        print("FIREBASE_TOKEN ${value!}");
+      });
+    } catch (ex) {
+      print(ex);
+    }
+    channel = const AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      description: "I am description",
+      importance: Importance.high,
     );
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: true,
+      sound: true,
+    );
+    await messaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    try {
+      await messaging.subscribeToTopic('uranus');
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+    } catch (ex) {
+      print(ex);
+    }
+    FirebaseCrashlytics.instance.app.setAutomaticDataCollectionEnabled(true);
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  } catch (error) {
+    EasyLoading.showToast("No Internet connection detected.",
+        dismissOnTap: true);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+Future firebaseAppInitialize() async {
+  await Firebase.initializeApp();
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+    try {
+      var data = event.data;
+      flutterLocalNotificationsPlugin.show(
+        data.hashCode,
+        data.isNotEmpty ? data['title'].toString() : event.notification?.title,
+        data.isNotEmpty ? data['body'].toString() : event.notification?.body,
+        NotificationDetails(
+          android: AndroidNotificationDetails(
+            channel.id,
+            channel.name,
+            icon: '@mipmap/logo',
+          ),
+        ),
+      );
+    } catch (ex, s) {
+      FirebaseCrashlytics.instance.recordError(ex, s, reason: "FCM Error");
+    }
+  });
+  FirebaseMessaging.onMessageOpenedApp.listen((message) async {
+    gloablNavigatorKey.currentState!.pushNamed('/notificationpage');
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    if (connectivityResult == ConnectivityResult.mobile) {
+      firebaseAppInitialize();
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      firebaseAppInitialize();
+    }
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return ChangeNotifierProvider(
+      create: (context) => AppLocale(),
+      child: Consumer<AppLocale>(
+        builder: (context, locale, child) {
+          return MaterialApp(
+            navigatorKey: gloablNavigatorKey,
+            title: "SmartStaff",
+            theme: lightThemeData,
+            darkTheme: darkThemeData,
+            themeMode: ThemeMode.light,
+            home: SplashScreen(token: firebaseToken!),
+            debugShowCheckedModeBanner: false,
+            builder: EasyLoading.init(),
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            locale: locale.locale,
+            routes: const <String, WidgetBuilder>{},
+          );
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
